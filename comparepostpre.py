7b98109cd3bcccdc593bce7280ab25dd862bbdce
Max Hunter <nippoo@nippoo.net>
2015-05-08 18:01:24 +0100

    Graph changes and command-line arguments

--- comparepostpre.py
+++ comparepostpre.py
@@ -2,13 +2,14 @@ from correlations import *
 import kwiklib
 import matplotlib
 import matplotlib.pyplot as plt
+import sys
 
-post = kwiklib.Experiment("/home/nippoo/neurodata/Achilles/postsleep/achilles_postsleep_firsthour.kwik")
-pre = kwiklib.Experiment("/home/nippoo/neurodata/Achilles/presleep/achilles_presleep_firsthour.kwik")
+orig = kwiklib.Experiment(sys.argv[1])
+compare = kwiklib.Experiment(sys.argv[2])
 
-sm, clusters_1, cluster_groups_1, clusters_2, cluster_groups_2 = compare_clusterings(pre, post)
+sm, clusters_1, cluster_groups_1, clusters_2, cluster_groups_2 = compare_clusterings(orig, compare)
 
-invsm, invclusters_1, invcluster_groups_1, invclusters_2, invcluster_groups_2 = compare_clusterings(post, pre)
+invsm, invclusters_1, invcluster_groups_1, invclusters_2, invcluster_groups_2 = compare_clusterings(compare, orig)
 
 selected_cl_1 = (cluster_groups_1 == 2)
 selected_cl_2 = (cluster_groups_2 == 2)
@@ -37,23 +38,20 @@ invsm = invsm[sortidx, :]
 
 ylen, xlen = sm.shape
 
-print invsm.shape
-print sm.shape
-
 def patch(x, y, hatch, color, lw=0, alpha=1.):
     return matplotlib.patches.Rectangle((x-0.5, y-0.5), 1, 1,
                                         hatch=hatch, fill=False, color=color, lw=lw, alpha=alpha)
 
 plt.xticks(np.arange(xlen), cl_numbers_2)
 plt.yticks(np.arange(ylen), cl_numbers_1[sortidx])
-plt.xlabel("postsleep clusters")
-plt.ylabel("presleep clusters")
+plt.xlabel("orig clusters")
+plt.ylabel("compare clusters")
 
 plt.axis([-0.5, xlen-0.5, -0.5, ylen-0.5])
 # plt.imshow(sm, cmap='Reds', interpolation='nearest')
 
 plt.imshow(np.zeros(sm.shape), cmap='Greys', interpolation='nearest')
-plt.title('Similarity between post- and pre-sleep clusterings')
+# plt.title('Similarity between clusterings')
 
 ax = plt.gca()
 
@@ -69,11 +67,12 @@ for i in range(sm.shape[0]): # rows
 
     # if np.argmax(sm[i, :]) == np.argmax(invsm[i, :]):
     #     ax.add_patch(patch(np.argmax(sm[i, :]), i, '', "green", lw="3"))
-    if np.argmax(sm[i, :]) == np.argmax(invsm[i, :]):
-        ax.add_patch(patch(np.argmax(sm[i, :]), i, '', "green", lw="3"))
+    if np.argmax(invsm[:, np.argmax(sm[i, :])]) == i:
+        ax.add_patch(patch(np.argmax(sm[i, :]), i, '', "black", lw="2"))
 
 
 # set the limits of the plot to the limits of the data
 # plt.colorbar()
 
-plt.savefig('mostsim.png')
+#plt.savefig('mostsim.png')
+plt.show()
